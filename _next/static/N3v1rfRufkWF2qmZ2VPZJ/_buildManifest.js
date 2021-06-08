06fc8cb916021b4084b1415af8aaf63a40cd0ee4
luizinfpp <64104799+luizinfpp@users.noreply.github.com>
2021-06-07 22:29:09 -0300

    Add files via upload

---
+++ _next/static/N3v1rfRufkWF2qmZ2VPZJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-05893a646b4088f46b8b.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
